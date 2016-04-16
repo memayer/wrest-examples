db5925130840088dc0f77e10b3162bb8fc726efe
memayer <memayer@gmail.com>
2016-04-16 00:36:02 -0500

    Adding single battleship example

---
+++ battleship_single.py
@@ -0,0 +1,52 @@
+"""
+1. Add ships based on the size of the board (don't place any on top of each other)
+2. Make different sized ships (must vertically or horizontally touch)
+3. Make it two player (players choose where their ships go)
+"""
+
+
+from random import randint
+
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Let's play Battleship!"
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+#print ship_row
+#print ship_col
+
+for turn in range(4):
+    print "Turn", turn + 1
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        break
+    else:
+        if (guess_row not in range(len(board))) or (guess_col not in range(len(board[0]))):
+            print "Oops, that's not even in the ocean."
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = "X"
+        print_board(board)
+        if turn == 3:
+            print "Game Over"
+
